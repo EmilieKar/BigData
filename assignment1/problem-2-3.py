f478c00f5e2543b463895166da9e83c59776917b
EmilieKar <emilie.karon.klefbom@gmail.com>
2022-03-24 13:16:02 +0100

    2-3 och perf i 2-1

--- assignment1/problem-2-3.py
+++ assignment1/problem-2-3.py
@@ -2,12 +2,11 @@ import multiprocessing # See https://docs.python.org/3/library/multiprocessing.h
 import argparse # See https://docs.python.org/3/library/argparse.html
 import random
 from math import pi
-from time import perf_counter
 
-def sample_pi(n):
+def sample_pi(n, seed):
     """ Perform n steps of Monte Carlo simulation for estimating Pi/4.
         Returns the number of sucesses."""
-    random.seed()
+    random.seed(seed)
     print("Hello from a worker")
     s = 0
     for i in range(n):
@@ -19,28 +18,17 @@ def sample_pi(n):
 
 
 def compute_pi(args):
-    start = perf_counter()
-    random.seed(1)
+    
     n = int(args.steps / args.workers)
-
+    
     p = multiprocessing.Pool(args.workers)
-    pStart = perf_counter()
-    s = p.map(sample_pi, [n]*args.workers)
-    pEnd = perf_counter()
+    s = p.starmap(sample_pi, [(n,(i+args.seed)) for i in range(args.workers)])
 
     n_total = n*args.workers
     s_total = sum(s)
     pi_est = (4.0*s_total)/n_total
     print(" Steps\tSuccess\tPi est.\tError")
     print("%6d\t%7d\t%1.5f\t%1.5f" % (n_total, s_total, pi_est, pi-pi_est))
-    end = perf_counter()
-    
-    pTime = pEnd - pStart
-    totTime = end - start
-    sTime = totTime - pTime
-    print(f'{start}, {pStart}, {pEnd},{end} ')
-    print("proportion of time spent in the serial section %f" % (sTime/totTime))
-    print("proportion of time spent in the paralell section %f" % (pTime/totTime))
 
 
 if __name__ == "__main__":
@@ -53,5 +41,9 @@ if __name__ == "__main__":
                         default='1000',
                         type = int,
                         help='Number of steps in the Monte Carlo simulation')
+    parser.add_argument('--seed',
+                        default='1',
+                        type = int,
+                        help='Random seed for generator')
     args = parser.parse_args()
     compute_pi(args)
