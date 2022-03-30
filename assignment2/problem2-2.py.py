f6f1ff464f9ceed6c35b3f088a7e97d8695a5fbe
Filip Borg <filipborg97@gmail.com>
2022-03-30 23:58:22 +0200

    Add steps parameter for workers
    Enables workers to exit without waiting for the queue to become empty
    Aslo adds some debugg prints

--- assignment2/problem2-2.py.py
+++ assignment2/problem2-2.py.py
@@ -4,19 +4,22 @@ import random
 from math import pi
 import time
 
-def sample_pi(queue, ret_queue, seed):
+def sample_pi(n, queue, ret_queue, seed):
     """ Perform n steps of Monte Carlo simulation for estimating Pi/4.
         Returns the number of sucesses."""
     random.seed(seed)
+    ret_queue.cancel_join_thread() #let the process exit without waiting for ret_queue to become empty
+    #print(f'{multiprocessing.current_process().name} created')
 
     while True:
         msg = queue.get()
         if msg == 'STOP':
+            #print(f'{multiprocessing.current_process().name} quits')
             #print(f'Worker recived message {msg}')
             break
         
         s = 0
-        for i in range(10000):
+        for i in range(n):
             x = random.random()
             y = random.random()
             if x**2 + y**2 <= 1.0:
@@ -25,7 +28,7 @@ def sample_pi(queue, ret_queue, seed):
         ret_queue.put(s)
     
 # Adds jobs to queue if accuracy < desired accuracy 
-# Adds "DONE" to queue if desired accuracy is reached
+# Adds "STOP" to queue if desired accuracy is reached
 def compute_pi(queue, ret_queue, step, acc, nmb_workers):
     s = 0
     n = 0
@@ -54,20 +57,36 @@ def run(args, workers = None):
         queue.put('DO')
 
     # Start worker processes
+    worker_list = []
     for i in range(workers - 1):
-        multiprocessing.Process(target=sample_pi, args=(queue, ret_queue, i + args.seed)).start()
+        curr_worker = multiprocessing.Process(target=sample_pi, args=(args.steps, queue, ret_queue, i + args.seed), daemon=True)
+        curr_worker.start()
+        worker_list.append(curr_worker)
 
+    #print(worker_list)
     pi_est, n = compute_pi(queue, ret_queue, args.steps, args.accuracy, workers)
-    #print(f'program finished with estimation {pi_est} using {n} samples')
+        
+
+    [w.join() for w in worker_list]
+    
+    #print(worker_list)
+    
+    print(f'program finished with estimation {pi_est} using {n} samples')
     return n
 
 def time_run(args, k_list):
     ret_list = []
+    base_samp_per_sec = None
     for k in k_list:
         start_time = time.time()
         n = run(args, k)
         stop_time = time.time()
-        ret_list.append((stop_time-start_time)/(n /10000))
+        ret_list.append((stop_time-start_time)/(n /args.steps))
+        
+        #print for debugg
+        if base_samp_per_sec is None:
+            base_samp_per_sec = n/(stop_time-start_time)
+        #print(f'workers:{k}, samples per second {n/(stop_time-start_time)}, speedup:{n/(stop_time-start_time)/base_samp_per_sec}, number of worker computations needed:{n /args.steps}')
     return ret_list
 
 if __name__ == "__main__":
