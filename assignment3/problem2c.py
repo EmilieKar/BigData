10886d02431eea786d2622e704addb7aaee3c91b
Filip Borg <fborg@bayes.ita.chalmers.se>
2022-04-19 09:49:13 +0200

    Finish the code for problem 2c

--- assignment3/problem2c.py
+++ assignment3/problem2c.py
@@ -11,52 +11,12 @@ from operator import add
 #their minimal and maximal value,
 #as well as the counts necessary for producing a histogram; i.e. for how many records does the value fall into a specific bin.
 class ValueStats(MRJob):
-    """
-    def configure_args(self):
-        super(ValueStats, self).configure_args()
-        self.add_passthru_arg(
-            '--bins', type=int, default=10, help='Bins for the bucketization')
-    """
-
-    """
-    def __init__(self, args):
-        print(args)
-        buckets = 10
-
-        if '-b' in args:
-            ind = args.index('-b')
-            buckets = int(args[ind + 1])
-            args = args[:ind] + args[ind + 2:]
-
-        self.buckets = buckets
-        print(args)
-
-        super().__init__(None)
-    """
-
-    """
-    def mapper_init(self):
-        self.values = defaultdict(int)
-
-    def mapper(self, _, line):
-        value = float(line.split()[2])
-        self.values[value] += 1
-
-    def mapper_final(self):
-        values_sum = sum(v*n for (v,n) in self.values.items())
-        values_terms = sum(self.values.values())
-        print(values_sum, values_terms)
-        yield ('sum_terms', (values_sum, values_terms))
-        yield ('max', max(self.values.keys()))
-        yield ('min', min(self.values.keys()))
-    """
     def mapper_s1(self, _, line):
         value = float(line.split()[2])
         yield ('s1_map_value', value)
 
     def combiner_s1(self, key, values):
         values = list(values)
-        #counter = Counter(values)
         part_sum = sum(values)
         part_sum_sq = sum(map(lambda x: x**2, values))
         sum_terms = len(values)
@@ -83,14 +43,14 @@ class ValueStats(MRJob):
 
         for value_list in stats_list[0]:
             for value in value_list:
-                yield ('s1_result', (value, mean, st_dev, max_val, min_val))
+                yield ('s1_result', (value, mean, st_dev, max_val, min_val, median_val))
 
 
     def bin_mapper(self, _, values):
-        [value, mean, st_dev, max_val, min_val] = values
+        [value, mean, st_dev, max_val, min_val, median] = values
         bins = np.histogram(value, range=(min_val, max_val), bins=10)[0].tolist()
 
-        yield ('bin_map', (bins, mean, st_dev, max_val, min_val))
+        yield ('bin_map', (bins, mean, st_dev, max_val, min_val, median))
 
     def bin_combiner(self, _, values):
         stats_list = list(zip(*values))
@@ -98,9 +58,10 @@ class ValueStats(MRJob):
         st_dev = stats_list[2][0]
         max_val = stats_list[3][0]
         min_val = stats_list[4][0]
+        median = stats_list[5][0]
         bins = np.sum(np.array(stats_list[0], dtype=int), axis=0).tolist()
 
-        yield ('bin_comb', (bins, mean, st_dev, max_val, min_val))
+        yield ('bin_comb', (bins, mean, st_dev, max_val, min_val, median))
 
     def final_reducer(self, _, values):
         stats_list = list(zip(*values))
@@ -110,6 +71,7 @@ class ValueStats(MRJob):
         yield ('stdev', stats_list[2][0])
         yield ('max', stats_list[3][0])
         yield ('min', stats_list[4][0])
+        yield ('median', stats_list[5][0])
 
 
     def steps(self):
