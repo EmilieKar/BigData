18800fcdddd40e577920167aa7b19d07ca051071
Filip Borg <fborg@bayes.ita.chalmers.se>
2022-05-04 22:12:59 +0200

    Add first solution to problem 2c

---
+++ assignment5/problem2c.py
@@ -0,0 +1,29 @@
+import time
+import argparse
+import pybloom_live
+from collections import defaultdict
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Calculate average common prefix for text using spark')
+    parser.add_argument('--file',  '-f',
+                        type=str,
+                        required=True,
+                        help='The file run the program on')
+
+    args = parser.parse_args()
+
+    start = time.time()
+    bf = pybloom_live.BloomFilter(capacity=804_000_000, error_rate=0.0001)
+    count_dict = defaultdict(lambda: 1)
+
+    with open(args.file) as file:
+        content = file.read().splitlines()
+        for item in content:
+            if bf.add(item):
+                count_dict[item] += 1
+                #print("duplicate found ", item)
+
+    print(count_dict)
+
+    end = time.time()
+    print(f'Calculations finished in: {end-start}')
