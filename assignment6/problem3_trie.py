ca6144a17a53e3a0804ee90808780222c1cacfb0
Filip Borg <fborg@bayes.ita.chalmers.se>
2022-05-14 00:09:14 +0200

    Update trie sampling

--- assignment6/problem3_trie.py
+++ assignment6/problem3_trie.py
@@ -1,25 +1,63 @@
 import numpy
 import time
 import argparse
+import sys
+from collections import Counter
+from tqdm import tqdm
+import pprofile
 
 class TrieNode:
-    def __init__(self, endNode=False):
+    def __init__(self):
+        self.keys = [] #as tuples (char, index)
         self.children = []
         self.weights = []
-        self.endNode = endNode
+
+    def makeProbabilities(self):
+        #if there are no weights varibel then it is an end node
+        if self.weights:
+            w_sum = sum(self.weights)
+            self.prob = list(map(lambda x: x/w_sum, self.weights))
+            #call recursively
+            [n.makeProbabilities() for n in self.children]
+
+        return
+
+    def sample(self):
+        #if weights exist then we have self.prob is we have ran makeProbabilities
+        if self.weights:
+            char,index = rng.choice(self.keys, replace=True, p=self.prob)
+            return char + self.children[int(index)].sample()
+        return ''
+
 
 class SampleTrie:
     def __init__(self):
       self.root = TrieNode()
 
-    def insert(self, item, wheight):
+    def insert(self, item, weight):
         node = self.root
         for char in item:
+            #insert new char
+            if char not in list(map(lambda x: x[0], node.keys)):
+                node.keys.append((char, len(node.keys)))
+                node.children.append(TrieNode())
+                node.weights.append(weight)
+                node = node.children[-1]
 
+            #increase weight of existing char
+            else:
+                index = list(map(lambda x: x[0], node.keys)).index(char)
+                node.weights[index] += weight
+                node = node.children[index]
 
-    def sample(self):
-        pass
+    def makeProbabilities(self):
+        self.root.makeProbabilities()
 
+    def sample(self, n):
+        samples = []
+        for _ in range(n):
+            samples.append(self.root.sample())
+        return samples
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compare sample speeds of numpy choice and trie for samples from file')
@@ -38,32 +76,50 @@ if __name__ == '__main__':
     items = []
     prob = []
 
-    #build the datastructures
-    with open(args.file, "r") as file:
-        # Reading form a file
-        content = file.readlines()
-        for line in content:
-            data = line.split()
-            items.append(data[0])
-            prob.append(int(data[1]))
+    trie = SampleTrie()
 
-    all_sum = sum(prob)
-    prob = list(map(lambda x: x/all_sum, prob))
-    print(f'length items {len(items)}')
-    print(f'length prob {len(prob)}')
-    
-    #the reference code
-    rng = numpy.random.default_rng()
-    start = time.time()
-    samples = rng.choice(items, args.n, replace=True, p=prob)
-    end = time.time()
+    #debug profile TODO remove
+    profiler = pprofile.Profile()
+    with profiler:
+        startSetup = time.time()
+        #build the datastructures
+        with open(args.file, "r") as file:
+            # Reading form a file
+            content = file.readlines()
+            for line in tqdm(content):
+                data = line.split()
+                item = data[0]
+                weight = int(data[1])
+                items.append(item)
+                prob.append(weight)
 
-    print(f' {args.n} samples in {end-start}s, {args.n/(end - start)} samples per second')
-    
-    #cleanup
-    del(items)
-    del(prob)
-    del(samples)
+                trie.insert(item, weight)
 
+        all_sum = sum(prob)
+        prob = list(map(lambda x: x/all_sum, prob))
 
-    #the trie code
+        trie.makeProbabilities()
+        endSetup = time.time()
+        print(f'startup took {endSetup-startSetup}s')
+
+        #the reference code
+        rng = numpy.random.default_rng()
+        start = time.time()
+        samples = rng.choice(items, args.n, replace=True, p=prob)
+        #print(samples)
+        end = time.time()
+        
+        print(f' {args.n} samples using numpy choice in {end-start}s, {args.n/(end - start)} samples per second')
+
+        #TODO remove
+        print(trie.root.keys)
+
+        #the trie code
+        startTrie = time.time()
+        samples_trie = trie.sample(args.n)
+        #print(samples_trie )
+        endTrie = time.time()
+        print(f' {args.n} samples using trie in {endTrie-startTrie}s, {args.n/(endTrie - startTrie)} samples per second')
+
+    #end profiling TODO remove
+    profiler.dump_stats("./tmp/profiler_stats.txt")
